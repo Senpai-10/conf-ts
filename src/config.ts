4f70bc972ad49d3b393dbe46e25417d8897326b0
abdalrzag eisa <bmjfdrh@gmail.com>
2022-06-08 02:58:41 +0300

    test and config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,33 @@
-export class Config {}
+interface Opts {
+    file_name?: string;
+    /** Config without file
+     *
+     * Example:
+     *
+     *      config: "[main] \n test = true"
+     *
+     * The parser will parse config and ignore file_name
+     */
+    config?: string;
+}
+
+export class Config {
+    opts: Opts;
+
+    constructor(opts: Opts) {
+        this.opts = opts;
+    }
+
+    public set() {}
+    public get() {}
+    public remove() {}
+
+    public empty() {}
+    /** Set config file back to default values */
+    public default() {}
+
+    /**
+     * Parser config (note: run inside constructor)
+     */
+    private parser() {}
+}
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,1 +1,9 @@
 import { Config } from "../src/index";
+
+describe("Config", () => {
+    const config = new Config({ config: "[main]\ntest=true" });
+
+    it("Config init", () => {
+        expect(config).toBeInstanceOf(Config);
+    });
+});
