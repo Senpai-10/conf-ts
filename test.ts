966e15e2033a2bd07d470a54422a91934f0a6713
abdalrzag eisa <bmjfdrh@gmail.com>
2022-06-09 09:09:00 +0300

    move test.ts out of tests

---
+++ test.ts
@@ -0,0 +1,11 @@
+import { Config } from "../src";
+
+const config = new Config({
+    config_str: `
+    dir = example/ hi this is me
+    [global]
+    # this is a comment
+    test=true`,
+});
+
+console.log(config.get("global.test"));
